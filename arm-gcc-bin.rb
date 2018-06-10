f9fb8d17b1966cc87032075f7a1640e3cf3849cb
Preston Garrison III <prestongarrisoniii@Dads-Old-MacBook-Pro.local>
2018-06-10 01:31:25 -0700

    6.3

--- arm-gcc-bin.rb
+++ arm-gcc-bin.rb
@@ -3,7 +3,7 @@ require 'formula'
 class ArmGccBin < Formula
 
     homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm'
-    url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-major-mac.tar.bz2'
+    url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-mac.tar.bz2?revision=ef567310-404c-48e2-8b67-12e3c86f584a?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Mac%20OS%20X,6-2017-q2-update'
     sha256 '7d3080514a2899d05fc55466cdc477e2448b6a62f536ffca3dd846822ff52900'
     version '6-2017-q2-major'
 
